8660becab2cb1b44b4e1e10eb59815a612c026c0
NiramayThaker <thakerniramay@gmail.com>
2025-01-16 19:37:57 +0530

    Bundling multiple JS file, HTMX, and AlpineJS

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,12 @@
+import "./helper1.js";
+import "./helper2.js";
+import Alpine from 'alpinejs'
+
+window.Alpine = Alpine
+Alpine.start()
+
+window.htmx = require('htmx.org');
+
 window.addEventListener('load', () => {
     document.getElementById("message").textContent = 'Rebundle JS'
 })
