bcfadff25e4e8dfb1b8e39ab10be8e9a89587d22
NiramayThaker <thakerniramay@gmail.com>
2025-01-16 13:04:06 +0530

    Setting up django and webpack

--- webpack/settings.py
+++ webpack/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -110,6 +111,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
